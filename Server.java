fa79440718a1ed08dabf8c67b6be427324b63dfd
mahaveerjan <mahaveerjanagouda2002@gmail.com>
2024-09-02 17:01:27 +0530

    Upadted again

--- Server.java
+++ Server.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,19 +9,37 @@ public class Server{
 
     public void run() throws IOException {
         int port =8010;
-        ServerSocket socket = new ServerSocket(port);
-        socket.setSoTimeout(10000);
+            ServerSocket socket = new ServerSocket(port);
+            socket.setSoTimeout(10000);
         while (true) {
-            System.out.println("Server is listening on port" + port);
-            Socket acceptedConnection = socket.accept();
-            System.out.println("Connection accepted from client" + acceptedConnection.getRemoteSocketAddress());
-            PrintWriter
+            try {
+                System.out.println("Server is listening on port" + port);
+                Socket acceptedConnection = socket.accept();
+                System.out.println("Connection accepted from client" + acceptedConnection.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                toClient.println("Hello from the server");
+                toClient.close();
+                fromClient.close();
+                acceptedConnection.close();
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
 
         }
 
 
     }
     public static void main(String[] args){
-
+    Server server = new Server();
+        try{
+            server.run();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
